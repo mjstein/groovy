a01f09e7f16e4f65d517ba38c1ba1a486432c67e
msuddd <msuddd@gmail.com>
2016-05-23 17:12:56 +0100

    blah

--- pipelines.groovy
+++ pipelines.groovy
@@ -17,10 +17,9 @@ String get_from_git(){
 
 
 node {
-   def job_map = ['qa':get_from_git(), 'pre':get_from_git(), 'prd':get_from_git()]
 
 //   def jobname="${env.JOB_NAME}"
 //   echo "check out for ${jobname}"
 //   git url: 'https://github.com/mjstein/mjs-kubernetes.git'
-   for (item in job_map){ specify_stg(item.key , item.value)}
+    specify_stg('Qa' , get_from_git())
 }
