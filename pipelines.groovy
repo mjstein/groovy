a93ffe61478b7ddbe7f86a5165863b7bd699eaba
msuddd <msuddd@gmail.com>
2016-05-23 17:05:08 +0100

    blah

--- pipelines.groovy
+++ pipelines.groovy
@@ -12,15 +12,15 @@ void specify_stg(String name, Closure method = {}){
 }
 
 Closure get_from_git(){ 
-  return "sh 'ls'"
+  return "sh 'ls -a'"
 }
 
 
 node {
    def job_map = ['qa':get_from_git(), 'pre':get_from_git(), 'prd':get_from_git()]
 
-   def jobname="${env.JOB_NAME}"
-   echo "check out for ${jobname}"
-   git url: 'https://github.com/mjstein/mjs-kubernetes.git'
+//   def jobname="${env.JOB_NAME}"
+//   echo "check out for ${jobname}"
+//   git url: 'https://github.com/mjstein/mjs-kubernetes.git'
    for (item in job_map){ specify_stg(item.key , item.value)}
 }
