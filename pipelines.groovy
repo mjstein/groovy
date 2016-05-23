2235019aa9576bab6ebf55e565588464dce3887a
msuddd <msuddd@gmail.com>
2016-05-23 16:43:11 +0100

    blah

--- pipelines.groovy
+++ pipelines.groovy
@@ -17,24 +17,9 @@ Closure get_from_git(){
 
 
 node {
-   def job_map = ['qa':get_from_git()]
+   def job_map = ['qa':get_from_git(), 'pre':{echo "doing summit"}, 'prd':{echo "doing summat else"}]
 
    def jobname="${env.JOB_NAME}"
    echo "check out for ${jobname}"
    git url: 'https://github.com/mjstein/mjs-kubernetes.git'
    for (item in job_map){ specify_stg(item.key , item.value)}
-//   specify_stg('QA', {echo "hello"})
-
-//   specify_stg('Pre')
-
-//   specify_stg('Prd')
-   
-//   int i = return_random(rand)
-//   if (dotests == 'true') {
-//     echo "${dotests} ${i}"
-//     stage 'Test'
-//     input "Proceed Test"
-//     step([$class: 'Fingerprinter', targets: '*'])
-//     echo 'Test Stage'
-//   }
-}
